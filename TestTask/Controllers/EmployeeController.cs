2960c3295d2a16f42362833608124ede1eccbea9
909702 <909702@LTIN230183>
2022-04-09 13:30:22 +0530

    Add project files.

---
+++ TestTask/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestTask.Models;
+
+namespace TestTask.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(new Salary());
+        }
+
+        [HttpPost]
+        public ActionResult Info(Salary s)
+        {
+            if (s.AnnualSalaryInUSD <= 18200)
+            {
+                s.Tax = 0;
+            }
+            else if(s.AnnualSalaryInUSD>=18201 && s.AnnualSalaryInUSD<=37000)
+            {
+                s.Tax = (int) Math.Round(((s.AnnualSalaryInUSD - 18200) * 0.19)/12);
+            }
+            else if(s.AnnualSalaryInUSD>=37001 && s.AnnualSalaryInUSD<=87000)
+            {
+                s.Tax = (int) Math.Round((((s.AnnualSalaryInUSD - 37000) * 0.325) + 3572)/12);
+            }
+            else if(s.AnnualSalaryInUSD >= 87001 && s.AnnualSalaryInUSD <= 180000)
+            {
+                s.Tax = (int) Math.Round((((s.AnnualSalaryInUSD - 87000) * 0.37) + 19822)/12);
+            }
+            else
+            {
+                s.Tax = (int) Math.Round((((s.AnnualSalaryInUSD - 180000) * 0.45) + 54232)/12);
+            }
+            s.GrossIncome = (int) Math.Round((double)s.AnnualSalaryInUSD  / 12 );
+            s.NetIncome = (int) Math.Round((double)(s.GrossIncome - s.Tax));
+            s.SuperAmount = (int) Math.Round((s.GrossIncome*s.SuperRateInPercentage)/100);
+
+            return View(s);
+        }
+    }
+}
---
+++ TestTask/Models/Salary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestTask.Models
+{
+    public class Salary
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int AnnualSalaryInUSD { get; set; }
+        public double Tax { get; set; }
+        public double NetIncome { get; set; }
+        public double GrossIncome { get; set; }
+        public double SuperRateInPercentage { get; set; }
+        public double SuperAmount { get; set; }
+        public DateTime Start_Date { get; set; }
+        public DateTime End_Date { get; set; }
+    }
+}
